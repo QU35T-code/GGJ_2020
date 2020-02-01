f03ad3f1780adecb06dc4ab7408a9281004331fb
Thomas Bernad <tbernad@localhost.localdomain>
2020-02-01 15:49:56 +0100

    [ADD/FIX] | options and credits

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@
 local is_menu = true
 local is_credit = false
 local is_music = false
+local is_option = false
 local h = 425
 success = love.window.setMode(1920, 1080)
 width = love.graphics.getWidth()
@@ -17,7 +18,9 @@ height = love.graphics.getHeight()
 	--background = love.graphics.newImage("")
 		y_credit = 700
 	    y_credit2 = 900
-	    son_credit = love.audio.newSource("credits.mp3", "stream")
+	    y_credit3 = 1100
+	    y_credit4 = 1300
+	    son_credit = love.audio.newSource("credits/credits.mp3", "stream")
 	end
 
 function mouse_pos(x, y)
@@ -27,12 +30,16 @@ function mouse_pos(x, y)
 				print("PLAY TOUCHED")
 			end
 			if (y >= 535 and y <= 630 and x >= 695 and x <= 1115 and button == 1) then
-				print("OPTION TOUCHED")
+				is_option = true
+				is_menu = false
+				is_music = true
+				is_credit = false
 			end
 			if (y >= 650 and y <= 745 and x >= 695 and x <= 1115 and button == 1) then
 				is_credit = true
 				is_music = true
 				is_menu = false
+				is_option = false
 			end
 			if (y >= 760 and y <= 855 and x >= 800 and x <= 1055 and button == 1) then
 				love.event.quit(false)
@@ -54,13 +61,24 @@ end
 			if (y_credit > -90) then
 		        y_credit = y_credit - 1
 		    else
-		        y_credit = 700
+		        y_credit = 1200
 		    end
 		    if (y_credit2 > -90) then
 		        y_credit2 = y_credit2 - 1
 		    else
-		        y_credit2 = 800
+		        y_credit2 = 1200
 		    end
+		    if (y_credit3 > -90) then
+		        y_credit3 = y_credit3 - 1
+		    else
+		        y_credit3 = 1200
+		    end
+		    if (y_credit4 > -90) then
+		        y_credit4 = y_credit4 - 1
+		    else
+		        y_credit4 = 1200
+		    end
+
 		end
 	end
 
@@ -70,6 +88,15 @@ function love.keypressed(key, scancode, isrepeat)
 	   if key == "escape" then
 	       is_credit = false
 	       is_music = false
+	       is_option = false
+	       is_menu = true
+	   end
+	end
+	if(is_option) then
+	   if key == "escape" then
+	       is_credit = false
+	       is_music = false
+	       is_option = false
 	       is_menu = true
 	   end
 	end
@@ -81,6 +108,13 @@ function love.keypressed(key, scancode, isrepeat)
 			is_credit = true
 			is_music = true
 			is_menu = false
+			is_option = false
+		end
+		if key == "return" and h == 535 then
+			is_credit = false
+			is_music = true
+			is_menu = false
+			is_option = true
 		end
 		if key == "down" and h <= 675 then
 			h = h + 110
@@ -103,10 +137,17 @@ end
 		end
 		if (is_credit) then
 	        love.audio.play(son_credit)
-	        love.graphics.printf("Alexis Martin", 500 * 2, y_credit, 100 * 2, "center")
-	        love.graphics.printf("Flavien Roche", 200 * 2, y_credit, 120 * 2, "center")
-	        love.graphics.printf("Thomas Bernad", 350 * 2, y_credit, 140 * 2, "center")
-	        love.graphics.printf("Neil Peyrot", 250 * 2, y_credit2, 120 * 2, "center")
-	        love.graphics.printf("Léo Soule", 400 * 2, y_credit2, 140 * 2, "center")
+	        love.graphics.printf("Alexis Martin", 500, y_credit, 1000, "center")
+	        love.graphics.printf("Flavien Roche", 500, y_credit2, 1000, "center")
+	        love.graphics.printf("Thomas Bernad", 500, y_credit3, 1000, "center")
+	        love.graphics.printf("Leo Soule", 500, y_credit4, 1000, "center")
     	end
+    	if (is_option) then
+    		love.graphics.setColor(255, 255, 255, 255)
+        	love.graphics.printf("You need to collect the differents pieces", 100, 120, 1800, "center")
+        	love.graphics.printf("in order the repair your time machine", 100, 350, 1800, "center")
+        	love.graphics.printf("and return to the right time", 100, 500, 1800, "center")
+        	love.graphics.setColor(150, 150, 0, 255)
+        	love.graphics.printf("Control your player with the arrows and jump with the space bar", 100, 750, 1800, "center")
+        end
 	end
